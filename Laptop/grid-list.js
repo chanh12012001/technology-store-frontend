b280017490b004fdbb6a4326c8eff03d66ed2a49
vohinhjjf <19520935@gm.uit.edu.vn>
2021-11-09 22:24:03 +0700

    update

---
+++ Laptop/grid-list.js
@@ -0,0 +1,9 @@
+
+var mySwiper = new Swiper('.cdt-brand-img swiper-container swiper-container-horizontal', {
+    direction: 'vertical',
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
---
+++ js/home_page/image_carousel.js
@@ -0,0 +1,16 @@
+$('.main-carousel').flickity({
+    cellAlign: 'left',
+    wrapAround: true,
+    autoPlay:true,
+});
+$('.main-carousel1').flickity({
+    cellAlign: 'left',
+    wrapAround: true,
+    autoPlay:true,
+});
+$('.main-carousel2').flickity({
+    cellAlign: 'left',
+    wrapAround: true,
+    autoPlay:true,
+});
+ 
