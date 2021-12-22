f19c8225e30cbb7d826ef219e79be3d65a31daaf
vohinhjjf <19520935@gm.uit.edu.vn>
2021-12-22 15:48:39 +0700

    gif
    gif_1

--- js/home_page/image_carousel.js
+++ js/home_page/image_carousel.js
@@ -3,28 +3,5 @@ $('.main-carousel').flickity({
     wrapAround: true,
     autoPlay:true,
 });
-<<<<<<< HEAD
-$('.main-carousel1').flickity({
-    cellAlign: 'left',
-    wrapAround: true,
-    autoPlay:true,
-});
-$('.main-carousel2').flickity({
-    cellAlign: 'left',
-    wrapAround: true,
-    autoPlay:true,
-});
-$('.main-carousel3').flickity({
-    cellAlign: 'left',
-    wrapAround: true,
-    autoPlay:true,
-});
-$('.main-carousel-dt').flickity({
-    cellAlign: 'left',
-    wrapAround: true,
-    autoPlay:true,
-});
-=======
->>>>>>> 34a20a430ea41d27c07bda54f23bc2735b8a314a
 
  
